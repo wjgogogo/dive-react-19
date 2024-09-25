60c2c403e802c162d359aabb86f5b843df6aa104
Wenjun Xu <906626481@qq.com>
2024-09-26 00:11:20 +0800

    docs: 初始化 docs site

---
+++ packages/rspress-site/README.md
@@ -0,0 +1,63 @@
+#react
+
+
+直接使用 19-rc 版本
+```bash
+gcl https://github.com/facebook/react.git src/react 
+```
+
+
+## Debug
+
+- Standardalone 直接启动
+- build source map -> 光
+- Kasong
+- Vite (create-react-app -> eject -> 修改配置)
+- Principal -> ignore dev validate
+
+
+## Catalogue
+- 准则
+- 架构预览
+- How to debug & use repo?
+- What's jsx? -> babel playground
+- Workflow
+	- Render
+		- Mount
+		- Update
+		- Reconciler
+			- Diff
+		- Bailout
+	- Commit
+	- LifeCycle
+- Hooks
+	- setState sync or async?
+	- Extend: How to use id for hooks?
+- Error handling
+	- Render
+	- Commit
+	- Extend: How to make a useErrorBoundary Hook?
+- Scheduler
+	- Mini-heap
+- Concurrent
+	- Lanes
+	- Suspense
+		- Offscreen
+		- Ping & Retry
+		- Use
+		- Lazy
+	- Concurrent hooks
+- Synthetic Event
+- Testing
+	- Act
+	- Testing utils
+- Devtools
+	- useRef for devtools: [https://github.com/facebook/react/issues/20394](https://github.com/facebook/react/issues/20394)
+	- Refresh
+	- Profiler
+	- V8 performance cliff
+- Compiler
+- Ecosystem
+	- Redux
+	- Valtio
+- 参考
--- packages/rspress-site/rspress.config.ts
+++ packages/rspress-site/rspress.config.ts
@@ -1,18 +1,46 @@
-import * as path from 'path';
-import { defineConfig } from 'rspress/config';
+import * as path from "path";
+import { defineConfig } from "rspress/config";
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: 'Rspress',
-  description: 'Rspack-based Static Site Generator',
-  icon: '/rspress-icon.png',
+  root: path.join(__dirname, "docs"),
+  title: "Rspress",
+  description: "Rspack-based Static Site Generator",
+  icon: "/rspress-icon.png",
   logo: {
-    light: '/rspress-light-logo.png',
-    dark: '/rspress-dark-logo.png',
+    light: "/rspress-light-logo.png",
+    dark: "/rspress-dark-logo.png",
   },
   themeConfig: {
     socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+      {
+        icon: "github",
+        mode: "link",
+        content: "https://github.com/wjgogogo/dive-react-19",
+      },
+      {
+        icon: "wechat",
+        mode: "img",
+        content: "/qrcode.jpg",
+      },
     ],
+    nav: [
+      {
+        text: "源码解析",
+        link: "/guide/",
+        activeMatch: "/guide/",
+      },
+      {
+        text: "API",
+        link: "https://rspress.dev/api/index.html",
+      },
+    ],
+    sidebar: {
+      "/guide/": [
+        {
+          text: "源码解析",
+          items: [{ text: "什么是jsx？", link: "/guide/01-what-is-jsx" }],
+        },
+      ],
+    },
   },
 });
